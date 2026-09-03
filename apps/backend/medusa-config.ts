5aa52287699af15201d5f2779313f62b38365589
Ugur Dogan <dgn.ugur@gmail.com>
2026-09-03 10:42:13 +0300

    Configure Medusa production environment

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    redisPrefix: "medusa-demo:",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -12,5 +14,18 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
     }
-  }
+  },
+  admin: {
+    vite: (config) => ({
+      ...config,
+      server: {
+        ...config.server,
+        allowedHosts: [
+          "localhost",
+          "127.0.0.1",
+          "api.demo.dijitalpanter.com",
+        ],
+      },
+    }),
+  },
 })
