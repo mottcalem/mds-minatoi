19b20f89c74c2f3c28e45572e5161e739aabc548
Ugur Dogan <dgn.ugur@gmail.com>
2026-09-03 11:17:54 +0300

    Adjust homepage slider to landscape viewport

--- apps/storefront/src/modules/home/components/hero/index.tsx
+++ apps/storefront/src/modules/home/components/hero/index.tsx
@@ -4,15 +4,17 @@ import Image from "next/image"
 const Hero = () => {
   return (
     <section className="overflow-hidden border-b border-[#ded8ce] bg-[#f3ece3]">
-      <LocalizedClientLink href="/store" className="block">
+      <LocalizedClientLink
+        href="/store"
+        className="relative block h-[58vw] min-h-[300px] max-h-[520px] small:h-[38vw] small:min-h-[360px] small:max-h-[610px]"
+      >
         <Image
           src="/images/slider-minatoi.jpg"
           alt="Minatoi cam tablo modelleri"
-          width={1671}
-          height={775}
+          fill
           priority
           sizes="100vw"
-          className="h-auto w-full"
+          className="object-cover object-center"
         />
       </LocalizedClientLink>
     </section>
